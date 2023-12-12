c6cccaa0e013feb48b16eaf74808076b7154ad7e
romin <romin48ppsv@gmail.com>
2023-12-12 16:04:03 +0530

    error handling

--- Backend/app.js
+++ Backend/app.js
@@ -5,11 +5,23 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 require('dotenv').config();
 var cors = require('cors');
-
+const cookieParser = require('cookie-parser');
+const errorHandler = require('./middleware/error');
 
 //connection
 mongoose.connect(process.env.DATABASE).then(()=> console.log("DB Connected"))
 .catch((err)=> console.log(err));
+
+//middleware
+app.use(morgan('dev'));
+app.use(bodyParser.json({limit: '5mb'}));
+app.use(bodyParser.urlencoded({limit: '5mb', extended: true}));
+app.use(cookieParser()); //for authentication
+app.use(cors());
+
+
+//error handling middleware
+app.use(errorHandler);
 
 //port 
 const port = process.env.PORT || 9000;
---
+++ Backend/middleware/error.js
@@ -0,0 +1,27 @@
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, req, res, next) => {
+    let error = {...err};
+    error.message = err.message;
+    if(err.name === 'CastError'){
+        const message = `Response not found ${err.value}`;
+        error = new ErrorResponse(message, 404);
+    }
+
+    if(err.name === 11000){
+        const message = `Duplicate field value entered`;
+        error = new ErrorResponse(message, 400);
+    }
+
+    if(err.name === 'ValidationError'){
+        const message = Object.values(err.errors).map(val => ' '+ val.message);
+        error = new ErrorResponse(message, 400);
+    }
+
+    res.status(error.statusCode || 500).json({
+        success: false,
+        error: error.message || 'server error'
+    })
+}
+
+module.exports = errorHandler;
